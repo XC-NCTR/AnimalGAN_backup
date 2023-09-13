6c5d39861cff4c0658e5b02941d1a3b0b8ea384a
XC-NCTR <83054794+XC-NCTR@users.noreply.github.com>
2023-09-13 11:46:31 -0500

    Update getSDF.py

--- getSDF.py
+++ getSDF.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pubchempy as pcp
 
 path = r'./'
-cids = pd.read_csv(os.path.join(path, 'deepcarc_compounds.csv'), dtype=str, usecols=['CID']).CID.to_list()
+cids = pd.read_csv(os.path.join(path, 'compounds.csv'), dtype=str, usecols=['CID']).CID.to_list()
 for cid in cids:
     sdfpath = os.path.join(path, 'SDFs', "{}.sdf".format(cid))
     try:
